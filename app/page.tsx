47fa158373bd0fe37f8c6827d3210a80dc8be064
TheMVProgrammer <franklin0415@hotmail.com>
2024-08-11 16:37:29 -0400

    feat: :sparkles: starting patient form

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { PatientForm } from "@/components/forms";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   const currentYear = new Date().getFullYear();
 
   return (
     <div className="flex h-screen max-h-screen">
+      {/* TODO: OTP Verification | PasskeyModal */}
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image
@@ -21,9 +23,19 @@ export default function Home() {
             <p className="justify-items-end text-dark-600 xl:text-left">
               © {currentYear} CarePulse
             </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
           </div>
         </div>
       </section>
+      <Image
+        src="/assets/images/onboarding-img.png"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[50%]"
+      />
     </div>
   );
 }
